4a7abab20893b7d7729e7d74191fd032eacb22d5
AdityaRaikwar <raikwaraditya555@gmail.com>
2025-08-03 10:34:55 +0530

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/Springboot/Kirana_Inventory_Management/KiranaInventoryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.Springboot.Kirana_Inventory_Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiranaInventoryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiranaInventoryManagementApplication.class, args);
+	}
+
+}
